f174a7c51ec209ecd3dd49e3d048d4160c50e3fc
Aleksandr Shatskikh <work@errand.ru>
2021-09-03 19:07:25 +0300

    refactor: js

--- static/src/js/openSearch.js
+++ static/src/js/openSearch.js
@@ -1,10 +1,26 @@
 'use strict';
 
-(() => {
+window.onload = () => {
 // Писать код отсюда
-  document.querySelector('.search-block .btn-search').addEventListener('click', function () {
-    document.querySelector('.search-row').classList.toggle('open-search');
-  })
+
+  const buttonOpen = document.querySelector('.search-block .icon-search'),
+        searchRow = document.querySelector('.search-row');
+
+  window.addEventListener('click', (e) => {
+    console.log(e.target);
+
+    if (e.target ===  buttonOpen) {
+      searchRow.classList.add('open-search');
+      //TODO Add focus
+    } else if(!e.target.closest('.search-row')) {
+      searchRow.classList.remove('open-search');
+    }
+
+  });
+
+
+
+
 
   document.querySelector('.header .menu').addEventListener('click', function () {
     document.querySelector('.menu-bar').classList.toggle('open-menu-bar');
@@ -15,5 +31,4 @@
   })
 
 // Писать код до сюда
-})();
-
+}
