7234bc987ca7aca05abd88bfc8e8f712317feaf3
Aleksandr Shatskikh <work@errand.ru>
2021-09-04 14:12:59 +0300

    refactor: js

--- static/src/js/openSearch.js
+++ static/src/js/openSearch.js
@@ -3,31 +3,32 @@
 window.onload = () => {
 // Писать код отсюда
 
-  const buttonOpen = document.querySelector('.search-block .icon-search'),
-        searchRow = document.querySelector('.search-row');
+  const openSearch = document.querySelector('.search-block .icon-search'),
+      searchRow = document.querySelector('.search-row'),
+      openMenu = document.querySelector('.header .menu'),
+      menuBar = document.querySelector('.menu-bar'),
+      menuClose = document.querySelector('.menu-bar .btn-close');
 
   window.addEventListener('click', (e) => {
-    console.log(e.target);
 
-    if (e.target ===  buttonOpen) {
+    if (e.target ===  openSearch) {
       searchRow.classList.add('open-search');
       //TODO Add focus
     } else if(!e.target.closest('.search-row')) {
       searchRow.classList.remove('open-search');
     }
 
+    if(e.target ===  openMenu || e.target.closest('.header .menu') === openMenu) {
+      menuBar.classList.add('open-menu-bar');
+    } else if(!e.target.closest('.menu-bar')) {
+      menuBar.classList.remove('open-menu-bar');
+    }
+
   });
 
 
-
-
-
-  document.querySelector('.header .menu').addEventListener('click', function () {
-    document.querySelector('.menu-bar').classList.toggle('open-menu-bar');
-  })
-
-  document.querySelector('.menu-bar .btn-close').addEventListener('click', function () {
-    document.querySelector('.menu-bar').classList.remove('open-menu-bar');
+  menuClose.addEventListener('click', function () {
+    menuBar.classList.remove('open-menu-bar');
   })
 
 // Писать код до сюда
