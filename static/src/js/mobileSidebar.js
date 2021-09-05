98adb6076136c2a9654b055f8fac8683c65f3e8c
Web_line <ekuksa1709@gmail.com>
2021-09-05 17:53:23 +0300

    Fixed classes all pages

--- static/src/js/mobileSidebar.js
+++ static/src/js/mobileSidebar.js
@@ -4,7 +4,7 @@ window.onload = () => {
 // Писать код отсюда
 
 const lionsBuner = document.querySelector('.media-info-widget'),
-    heroBlock = document.querySelector('.hero-publications');
+    heroBlock = document.querySelector('.hero-post');
 
 if(window.innerWidth < 760) {
   heroBlock.append(lionsBuner);
