e09128affc867beb94b68be4966851efdb2fcc0b
Aleksandr Shatskikh <work@errand.ru>
2021-09-05 21:53:37 +0300

    feat: mobileSidebar script refactor

--- static/src/js/mobileSidebar.js
+++ static/src/js/mobileSidebar.js
@@ -4,10 +4,14 @@ window.onload = () => {
 // Писать код отсюда
 
 const lionsBuner = document.querySelector('.media-info-widget'),
-    heroBlock = document.querySelector('.hero-post');
+      heroBlock = document.querySelector('.hero-publications'),
+      allPublications = document.querySelector('.all-publications');
 
 if(window.innerWidth < 760) {
   heroBlock.append(lionsBuner);
+
+  console.log(heroBlock.querySelectorAll('.publication-full')[1])
+  allPublications.querySelector('.title-rows').after(heroBlock.querySelectorAll('.publication-full')[1])
 }
 
 
