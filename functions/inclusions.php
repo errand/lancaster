880b0f9531d4a7453654e2e942e47c6c57c0e8bf
shamagir <shamagir@gmail.com>
2021-09-06 22:36:02 +0300

    fix: add php for load more posts

--- functions/inclusions.php
+++ functions/inclusions.php
@@ -3,4 +3,5 @@
 	include_once( 'news.php' );
 	include_once( 'read_more_posts.php' );
 	include_once( 'user_avatar.php' );
+	include_once( 'read_more_posts.php' );
 	include_once( 'add_to_context.php' );
