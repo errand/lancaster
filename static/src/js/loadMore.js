6e32e3dad39b1eff23f036758253f6b024ad2ef3
shamagir <shamagir@gmail.com>
2021-09-08 01:45:20 +0300

    fix: only on pages with .all-post

--- static/src/js/loadMore.js
+++ static/src/js/loadMore.js
@@ -2,47 +2,49 @@
 
 (() => {
 
-  const container      = document.querySelector('.all-post');
-  const loadMoreButton = document.getElementById( 'loadMore' );
-  const request        = new XMLHttpRequest();
-  const postsPerPage   = container.dataset.postsPerPage;
+if( document.querySelector( '.all-post' )) {
+    const container      = document.querySelector('.all-post');
+    const loadMoreButton = document.getElementById( 'loadMore' );
+    const request        = new XMLHttpRequest();
+    const postsPerPage   = container.dataset.postsPerPage;
 
-  loadMoreButton.addEventListener( 'click', loadMore );
+    loadMoreButton.addEventListener( 'click', loadMore );
 
-  function loadMore() {
-    let paged  = container.dataset.paged;
-    let offset = container.dataset.offset;
-    let data   = `paged=${ paged }&offset=${ offset }`;
+    function loadMore() {
+      let paged  = container.dataset.paged;
+      let offset = container.dataset.offset;
+      let data   = `paged=${ paged }&offset=${ offset }`;
 
-    request.open( 'POST', `${ my_ajax_url.ajax_url }?action=loadMorePosts`, true );
-    request.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8' );
-    request.send( data );
+      request.open( 'POST', `${ my_ajax_url.ajax_url }?action=loadMorePosts`, true );
+      request.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8' );
+      request.send( data );
 
-    request.onprogress = () => {
-      console.log( 'The request is in progress' );
-    };
-    request.onload     = () => {
-      if ( request.status != 200 ) {
-        console.log( `Error ${ request.status }: ${ request.statusText }` );
-      } else {
-        let postsArray  = Array.from( container.querySelectorAll('.post-row') );
-        let lastPost    = postsArray[ postsArray.length - 1 ];
-        let content     = JSON.parse(request.response);
-        let total = content[ 'total' ];
-          content = content[ 'posts' ].replace( /[\n\r\t]/g, '' );
-        
-        lastPost.insertAdjacentHTML( 'afterEnd', content );
-        container.dataset.paged  = parseInt( paged ) + 1;
-        container.dataset.offset = parseInt( offset ) + parseInt( postsPerPage );
-        console.log( total - offset ); 
-        if ( total - offset < postsPerPage ) {
-          loadMoreButton.style.display = 'none';
+      request.onprogress = () => {
+        console.log( 'The request is in progress' );
+      };
+      request.onload     = () => {
+        if ( request.status != 200 ) {
+          console.log( `Error ${ request.status }: ${ request.statusText }` );
+        } else {
+          let postsArray  = Array.from( container.querySelectorAll('.post-row') );
+          let lastPost    = postsArray[ postsArray.length - 1 ];
+          let content     = JSON.parse(request.response);
+          let total = content[ 'total' ];
+            content = content[ 'posts' ].replace( /[\n\r\t]/g, '' );
+          
+          lastPost.insertAdjacentHTML( 'afterEnd', content );
+          container.dataset.paged  = parseInt( paged ) + 1;
+          container.dataset.offset = parseInt( offset ) + parseInt( postsPerPage );
+          console.log( total - offset ); 
+          if ( total - offset < postsPerPage ) {
+            loadMoreButton.style.display = 'none';
+          }
         }
-      }
-    };
-    request.onerror = () => {
-      console.log( 'The request failed' );
-    };
+      };
+      request.onerror = () => {
+        console.log( 'The request failed' );
+      };
+    }
   } 
 
 })();
