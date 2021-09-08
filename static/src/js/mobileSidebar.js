2117c1b2730ac6d1fd3d6d9e6af041ef3a58b666
Aleksandr Shatskikh <work@errand.ru>
2021-09-08 15:49:45 +0300

    feat: mobile block positions

--- static/src/js/mobileSidebar.js
+++ static/src/js/mobileSidebar.js
@@ -3,17 +3,23 @@
 (() => {
   // Писать код отсюда
 
-  const lionsBuner = document.querySelector('.widget_lanc_memes_widget'),
-    heroBlock = document.querySelector('.hero-post'),
-    allPublications = document.querySelector('.all-post'),
-    linkWidget = document.querySelector('.link-widget');
+  if (document.querySelector('body').classList.contains('home') && window.innerWidth < 769) {
 
-  if (window.innerWidth < 769) {
+    const lionsBuner = document.querySelector('.widget_lanc_memes_widget'),
+        heroBlock = document.querySelector('.hero-post'),
+        allPublications = document.querySelector('.all-post'),
+        allPublicationsTitle = allPublications.querySelector('.title-rows'),
+        linkWidget = document.querySelector('.link-widget').closest('.widget');
+
     heroBlock.append(lionsBuner);
 
     allPublications.querySelector('.title-rows').after(heroBlock.querySelectorAll('.post-full')[1]);
     allPublications.querySelector('.post-full').classList.add('post-row');
     allPublications.querySelector('.post-full').classList.remove('post-full');
+
+
+    heroBlock.querySelector('.post-full').querySelector('.hero-category').before(allPublicationsTitle);
+    allPublications.before(linkWidget);
   }
 
   // Писать код до сюда
